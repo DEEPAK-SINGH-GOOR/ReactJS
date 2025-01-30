4ecff20679a2ca64eedf482745e6d1b9f7a3e70c
DEEPAK-SINGH <149694925+DEEPAK-SINGH-GOOR@users.noreply.github.com>
2025-01-30 16:52:25 +0530

    Update validation.jsx

--- Validation/src/validation.jsx
+++ Validation/src/validation.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod";
+import "./validation.css"
 
 const validation = z.object({
     name: z.string().min(2, "Min 2 characters !!").max(25, "Less than 25 characters !!"),
@@ -11,35 +12,30 @@ const validation = z.object({
         .regex(/[A-Z]/, "Capital characters Require !!")
         .regex(/[!@#$%&*]/, "Special characters !!")
 })
-
 const userFrom = () => {
-    const { register, handleSubmit, formState: { errors }, } = useForm({ resolver: zodResolver(validation), });
+    const { register, handleSubmit, formState: { errors }, } = useForm({ resolver: zodResolver(validation)});
 
     const onSubmit = (data) => {
         console.log("onSubmit", data, errors);
     };
     return (
-        <div>
+        <div className="container">
             <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="">name</label>
-                <input type="text" {...register("name")} />
+                <input type="text" {...register("name")} className={errors.name ? "error" :"success"}/>
                 {errors.name && <p>{errors.name.message}</p>}
                 <br />
                 <label>email: </label>
-                <input type="email" {...register("email")} />
+                <input type="email" {...register("email")} className={errors.email ?"error" : "success"}/>
                 {errors.email && <p>{errors.email.message}</p>}
                 <br />
                 <label htmlFor="">password</label>
-                <input type="text" {...register("password")} />
+                <input type="text" {...register("password")} className={errors.email ?"error" : "success"}/>
                 {errors.password && <p>{errors.password.message}</p>}
                 <br />
                 <input type="submit" />
             </form>
         </div>
     )
-
 }
-
-
-
 export default userFrom;
